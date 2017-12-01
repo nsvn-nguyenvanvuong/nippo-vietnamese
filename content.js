22693b8733023a6aa483cab4aeb4903e22d1c999
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2017-12-01 13:21:08 +0700

    update style & keyword

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ var dics = {
   'ログイン': 'Đăng nhập',
   '取消し': 'Làm lại',
   'ログアウトしました。': 'Đăng xuất thành công!',
-  '登　　録': 'Đăng ký',
+  '　登　　録　': 'Đăng ký',
   '日報入力＆照会': 'Báo cáo hàng ngày',
   '査閲画面': 'Trang đánh giá',
   'ＡＢＣ物件状況照会': 'Yêu cầu',
@@ -29,7 +29,7 @@ var dics = {
   'パスワード': 'Mật khẩu xác thực:',
   '業務': 'Dự án',
   '作業': 'Công việc',
-  '日付':'Ngày',
+  '日付': 'Ngày',
   '部門': 'Nhóm',
   '担当': 'Nhân viên',
   '時間帯': 'Giờ giấc báo cáo',
@@ -42,7 +42,33 @@ var dics = {
   '次回予定・所感': 'Thời gian biểu cho ngày hôm sau',
   '等を具体的に報告のこと': 'Vui lòng báo cáo cụ thể những mục trên',
   '日報処理': 'Công việc thường ngày',
-  '報告・相談': 'Báo cáo & trợ giúp'
+  '報告・相談': 'Báo cáo & trợ giúp',
+  /*'共通強化': 'Tăng cường chung',
+  '共通維持': 'Bảo trì thường',
+  '共通': 'Thường ngày',
+  '企画・基盤': 'Quy hoạch và nền tảng',
+  '共通': 'Thường',
+  '就業強化': 'Tăng cường việc làm',
+  '就業維持': 'Duy trì việc làm',
+  '就業': 'Việc làm',
+  '給与': 'Lương',
+  '給与(要定既存)': 'Mức lương (yêu cầu hiện có)',
+  '給与強化': 'Tăng lương',
+  '給与維持': 'Biên chế',
+  '人事': 'Nguồn nhân lực',
+  '人事(要定既存)': 'Nguồn nhân lực (bắt buộc hiện có)',
+  '人事強化': 'Tăng cường nguồn nhân lực',
+  '人事維持': 'Bảo trì Nhân sự',
+  'OH強化': 'Tăng cường OH',
+  'OH維持': 'Bảo trì OH',
+  'OH': 'OH',
+  'HRM強化': 'Tăng cường nhân sự',
+  'HRM維持': 'Duy trì HRM',
+  'HRM': 'HRM',*/
+  '詳細設計': 'Thiết kế chi tiết',
+  'AP開発': 'Chương trình',
+  'ｺｰﾃﾞｨﾝｸﾞ': 'đang viết',
+  '製造者ﾃｽﾄ': 'tự test'
 };
 
 
@@ -58,10 +84,11 @@ setInterval(function () {
       });
 
     for (var i in dics) {
+      var key = i.replace('(', '\(').replace(')', '\)');
       if ($item.html()) {
         let html = $item.html()
           .replace(new RegExp('&nbsp;', 'g'), '')
-          .replace(new RegExp(i, "g"), `<span>${dics[i]}</span>`)
+          .replace(new RegExp(key, "g"), `<span>${dics[i]}</span>`)
           .replace(new RegExp('<br', 'g'), '<div class="break"></div')
           .replace(new RegExp(`value="<span>${dics[i]}</span>"`, 'g'), `value="${dics[i]}"`);
 
